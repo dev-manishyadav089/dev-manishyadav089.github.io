ff37061ee5b3fa8f68c8c096f149a26f87439a81
Manish Yadav <manishyy089@gmail.com>
2026-01-13 01:44:07 +0530

    changes

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
-    // Make all sections visible by default
-    const sections = document.querySelectorAll("section");
-    sections.forEach(sec => {
-        sec.style.opacity = 1;
-        sec.style.transform = "translateY(0)";
-    });
+//     // Make all sections visible by default
+//     const sections = document.querySelectorAll("section");
+//     sections.forEach(sec => {
+//         sec.style.opacity = 1;
+//         sec.style.transform = "translateY(0)";
+//     });
 
-    // Button glow
-    const buttons = document.querySelectorAll(".btn");
-    buttons.forEach(btn => {
-        btn.addEventListener("mouseenter", () => {
-            btn.style.boxShadow = "0 0 15px rgba(255,255,255,0.6)";
-        });
-        btn.addEventListener("mouseleave", () => {
-            btn.style.boxShadow = "none";
-        });
-    });
+//     // Button glow
+//     const buttons = document.querySelectorAll(".btn");
+//     buttons.forEach(btn => {
+//         btn.addEventListener("mouseenter", () => {
+//             btn.style.boxShadow = "0 0 15px rgba(255,255,255,0.6)";
+//         });
+//         btn.addEventListener("mouseleave", () => {
+//             btn.style.boxShadow = "none";
+//         });
+//     });
 
-});
+// });
 
 function openEmail() {
-  const user = "manish";
+  const user = "manishyy089";
   const domain = "gmail.com";
   window.location.href = "mailto:" + user + "@" + domain;
 }
